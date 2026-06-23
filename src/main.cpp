5d09282a112bb9ca0102eb2e505182c674a61721
bstr1ckland <ben.strickland@dal.ca>
2026-06-23 17:50:52 -0300

    Started working on front end

--- src/main.cpp
+++ src/main.cpp
@@ -14,27 +14,46 @@ namespace fs = std::filesystem;
 
 int main()
 {
-    // FTXUI Documentation: https://arthursonzogni.github.io/FTXUI/
     using namespace ftxui;
- 
-    /** 
-    Element document = hbox({
-        text("left")   | border,
-        text("middle") | border | flex,
-        text("right")  | border,
-    });
-    
-    Screen screen = Screen::Create(
-        Dimension::Full(), 
-        Dimension::Fit(document)
-    );
-    
-    Render(screen, document);
-    screen.Print();
-    */
 
-    cout << get_total_ram() << endl;
-    cout << get_used_ram() << endl;
+    // CPU 
+    double max_cpu_freq = get_max_cpu_freq();
+    int core_count = get_core_count();
+    std::string cpu_name = get_cpu_name();
+
+    double avg_cpu_freq;
+    double cpu_usage;
+    double cpu_temperature;
+    std::vector<double> cpu_freq_per_core;
+    std::vector<double> cpu_usage_per_core;
+
+
+    // GPU 
+    double total_vram = get_total_vram();
+    std::string gpu_name = get_gpu_name();
+
+    double gpu_temp;
+    double gpu_usage;
+    double vram_usage;
+
+
+    // System 
+    std::string system_version = get_version();
+
+    double system_uptime;
+    double idle_time;
+
+
+    // RAM 
+    double total_ram = get_total_ram();
+
+    double used_ram;
+
+
+    while (true)
+    {
+        
+    }
     
     return 0;
 }
